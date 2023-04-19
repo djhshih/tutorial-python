abbac7a753fa0d0697bd3b5f864306a9edcb6fcb
David J. H. Shih <djh.shih@gmail.com>
2023-04-19 20:29:30 +0800

    Document source of data.

---
+++ data/get.sh
@@ -0,0 +1,5 @@
+curl -O http://swcarpentry.github.io/python-novice-gapminder/files/python-novice-gapminder-data.zip
+unzip -j python-novice-gapminder-data.zip
+
+# gapminder_tall.csv is renamed from gapminder_full.csv from
+# https://www.kaggle.com/datasets/tklimonova/gapminder-datacamp-2007
